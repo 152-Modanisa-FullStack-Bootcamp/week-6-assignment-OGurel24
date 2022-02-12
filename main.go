ba4d07474ec4e4bdc11ad6fd088e330e80056c8f
OGurel24 <onur.gurel@useinsider.com>
2022-02-12 23:34:26 +0300

    Service Layer

--- main.go
+++ main.go
@@ -1,134 +1,50 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"homework5/service"
 	"net/http"
 )
 
-type user struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Balance int    `json:"balance"`
-}
-
-var data = []user{
-	{ID: 1, Name: "onur", Balance: 999},
-	{ID: 2, Name: "ugur", Balance: 10},
-}
-
 func main() {
 
-	http.HandleFunc("/", getData)
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", mainController)
+	err := http.ListenAndServe(":8080", nil)
+	fmt.Println(err)
 
 }
 
-func getData(w http.ResponseWriter, r *http.Request) {
+func mainController(w http.ResponseWriter, r *http.Request) {
 	url := r.RequestURI
 
+	// Get Redirection
+	if r.Method == "GET" {
+		if url != "/" {
+			service.GetSpecificUser(w, r, url[1:])
+			return
+		}
+		service.GetAllUsers(w, r)
+		return
+	}
+
+	// Put Redirection
 	if r.Method == "PUT" {
-		addUser(w, r, url[1:])
+		service.AddUser(w, r, url[1:])
 		return
 	}
 
+	// Post Redirection
 	if r.Method == "POST" {
-		updateUser(w, r, url[1:])
+		service.UpdateUser(w, r, url[1:])
 		return
 	}
 
-	//should work with only GET
-	if r.Method != "GET" {
-		w.WriteHeader(501)
-		_, err := fmt.Fprintf(w, "Invalid request method")
-		if err != nil {
-			panic(err)
-		}
-		return
-	}
-
-	if url != "/" {
-		getSpecificData(w, r, url[1:])
-		return
-	}
-
-	users, err := json.Marshal(data)
-	if err != nil {
-		panic(err)
-	}
-	_, err = w.Write(users)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func getSpecificData(w http.ResponseWriter, r *http.Request, username string) {
-	fmt.Println(username)
-	for _, v := range data {
-		if username == v.Name {
-			user, _ := json.Marshal(v)
-			_, err := w.Write(user)
-			if err != nil {
-				panic(err)
-			}
-
-			return
-		}
-	}
-	w.WriteHeader(404)
-	fmt.Fprintf(w, "User could not be found!")
-	return
-}
-
-func addUser(w http.ResponseWriter, r *http.Request, username string) {
-	for _, v := range data {
-		if v.Name == username {
-			w.WriteHeader(417)
-			_, err := fmt.Fprintf(w, "User is already exist")
-			if err != nil {
-				panic(err)
-			}
-
-			return
-		}
-	}
-
-	newUser := user{len(data), username, 24}
-	data = append(data, newUser)
-	_, err := fmt.Fprintf(w, "User is added")
+	// Not implemented, return 501
+	w.WriteHeader(501)
+	_, err := fmt.Fprintf(w, "Invalid request method")
 	if err != nil {
 		panic(err)
 	}
 
-	return
-}
-
-func updateUser(w http.ResponseWriter, r *http.Request, username string) {
-	type updateInfo struct {
-		Balance int `json:"balance"`
-	}
-
-	for i, v := range data {
-		if v.Name == username {
-			decoder := json.NewDecoder(r.Body)
-			var t updateInfo
-			err := decoder.Decode(&t)
-			if err != nil {
-				panic(err)
-			}
-			data[i].Balance += t.Balance
-			_, err = fmt.Fprintf(w, "Balance is changed successfully!")
-			if err != nil {
-				panic(err)
-			}
-			return
-		}
-	}
-
-	w.WriteHeader(404)
-	_, err := fmt.Fprintf(w, "User could not be found!")
-	if err != nil {
-		panic(err)
-	}
 	return
 }
---
+++ service/service.go
@@ -0,0 +1,122 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type user struct {
+	Name    string `json:"name"`
+	Balance int    `json:"balance"`
+}
+
+var data = []user{
+	{Name: "onur", Balance: 999},
+	{Name: "ugur", Balance: 10},
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	// Collect all users and handle error if there is any
+	users, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	// Return all users and handle error if there is any
+	_, err = w.Write(users)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+}
+
+func GetSpecificUser(w http.ResponseWriter, r *http.Request, username string) {
+	index, exist := isUserExist(username)
+
+	if exist {
+		user, _ := json.Marshal(data[index])
+		_, err := w.Write(user)
+		if err != nil {
+			w.WriteHeader(500)
+		}
+
+		return
+	}
+
+	w.WriteHeader(404)
+	_, err := fmt.Fprintf(w, "User could not be found!")
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	return
+}
+
+func AddUser(w http.ResponseWriter, r *http.Request, username string) {
+	_, exist := isUserExist(username)
+
+	// If user is already exist return 417
+	if exist {
+		w.WriteHeader(417)
+		_, err := fmt.Fprintf(w, "User is already exist")
+		if err != nil {
+			w.WriteHeader(500)
+		}
+	}
+
+	// Otherwise add user
+	newUser := user{username, 24}
+	data = append(data, newUser)
+	_, err := fmt.Fprintf(w, "User is added")
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	return
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request, username string) {
+	index, exist := isUserExist(username)
+
+	//If user is not exist return 404
+	if !exist {
+		w.WriteHeader(404)
+		_, err := fmt.Fprintf(w, "User could not be found!")
+		if err != nil {
+			w.WriteHeader(500)
+		}
+
+		return
+	}
+
+	// Otherwise create a balance update info struct and update user balance
+	type updateInfo struct {
+		Balance int `json:"balance"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var t updateInfo
+	err := decoder.Decode(&t)
+
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	data[index].Balance += t.Balance
+	_, err = fmt.Fprintf(w, "Balance is changed successfully!")
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	return
+}
+
+// Helper function detects if the user is existed or not
+func isUserExist(username string) (int, bool) {
+
+	for i, v := range data {
+		if v.Name == username {
+			return i, true
+		}
+	}
+	return -1, false
+}
